448dd69a909f042e151768f67d17528d832b95f3
Daniel Angulo <dangulo4@gmail.com>
2020-02-24 21:53:24 -0800

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,44 @@
-const questions = [
+const inquirer  = require('inquirer');
+const fs        = require('fs');
+// const axios     = require('axios');
+// const util      = require('util');
 
-];
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your Github user name?'
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter a project title?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description?'
+        }
+    ]);
 
-function writeToFile(fileName, data) {
+// const questions = [
+
+// ];
 }
 
-function init() {
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName),
+    data);
+}
 
+async function init() {
+    console.log('The user has been prompt')
+    try {
+        await promptUser();
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 init();
