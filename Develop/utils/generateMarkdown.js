01afa653d3c0d364ef8bce877514f2723dbde413
Daniel Angulo <dangulo4@gmail.com>
2020-02-26 17:34:00 -0800

    path

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,7 +2,7 @@ function generateMarkdown(data) {
   return `# ${data.title}
   [![Build Status](https://dev.azure.com/vscode/VSCode/_apis/build/status/VS%20Code?branchName=master)](https://dev.azure.com/vscode/VSCode/_build/latest?definitionId=12)
   ## ${data.description}
-  ![](./utils/images/example.gif)
+  ![](/utils/images/example.gif)
   ## Table of Contents
   - [Installation](#Installation)
   - [Usage](#Usage)
