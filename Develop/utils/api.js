a13ef2103ec96f3a35cc6d6720c9f0259837ffbd
Daniel Angulo <dangulo4@gmail.com>
2020-02-26 21:36:15 -0800

    created api to retrieve github by user

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,41 +1,48 @@
 const axios     = require('axios');
 const inquirer  = require('inquirer');
-require('dotenv').config();
+
+return inquirer
+ .prompt({
+  type: 'input',
+  name: 'username',
+  message: 'What is your Github Username?'
+  
+ })
+
+ .then(function ({username}) {
+   const queryUrl = `https://api.github.com/users/${username}`;
+
+   axios
+   .get(queryUrl)
+   .then(function (res) {
+     console.log(res.data);
+   });
+ });
 
 
-getUser();
 
-async function getUser() {
-  try {
-    const {username} = await inquirer.prompt({
-      name: 'username',
-      message: 'What is your Github Username?'
-    });
+// getUser();
 
-    const {data} = await axios.get(
-      `https://api.github.com/users/${username}`
-     );
-    
-    console.log(data);
-
-    } catch (err) {
-      console.log('User does not exist');
-      console.log(err);
-    }
- }
-
-// const api = {
-//   getUser(username) {
-//     return axios
-//       .get(`https://api.github.com/users/${username}?client_id=${
-//         process.env.CLIENT_ID
-//       }&client_secret=${process.env.CLIENT_SECRET}`
-//     ).catch(err => {
-//       console.log('User not found')
-//       process.exit(1);
+// async function getUser() {
+//   console.log('The user has been prompt for github username')
+//   try {
+//     const {username} = await inquirer.prompt({
+//       type: 'input',
+//       message: 'What is your Github Username?',
+//       name: 'username'
+      
 //     });
 
-//   }
-// };
+//     const {github} = await axios.get(
+//       `https://api.github.com/users/${username}`
+//      );
+    
+//     console.log(github);
+
+//     } catch (err) {
+//       // console.log('User does not exist');
+//       console.log(err);
+//     }
+//  }
 
 module.exports = api;
