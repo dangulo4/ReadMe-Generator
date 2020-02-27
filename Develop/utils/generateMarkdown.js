f70d2ba622ca2881715c18e0e6cba4037f77d53e
Daniel Angulo <dangulo4@gmail.com>
2020-02-26 19:59:50 -0800

    gif

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,7 +2,7 @@ function generateMarkdown(data) {
   return `# ${data.title}
   [![Build Status](https://dev.azure.com/vscode/VSCode/_apis/build/status/VS%20Code?branchName=master)](https://dev.azure.com/vscode/VSCode/_build/latest?definitionId=12)
   ## ${data.description}
-  ![Questions(utils/images/example.gif)*Prompt User*][about https://github.com/dangulo4/ReadMe-Generator/tree/master/Develop]
+  ![Questions](utils/images/example.gif)*Prompt User*[about https://github.com/dangulo4/ReadMe-Generator/tree/master/Develop]
   ## Table of Contents
   - [Installation](#Installation)
   - [Usage](#Usage)
